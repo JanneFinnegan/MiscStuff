7890805bf027ee3368c1933c22be3f130b8dbd92
JanneFinnegan <42029004+JanneFinnegan@users.noreply.github.com>
2018-08-16 23:53:20 +0300

    Create kukkelikuu.php

---
+++ kukkelikuu.php
@@ -0,0 +1,14 @@
+<?php
+
+    #
+    #
+    # Creator: heidi.laajalahti@tuonelahub.fi
+    # Supporter: matti.kaskinen@tuonelahub.fi
+    #
+
+    $string1 = "Hello";
+    $string2 = "Project Persons....";
+    
+    print($string1 . " " . $string2);
+
+?>
